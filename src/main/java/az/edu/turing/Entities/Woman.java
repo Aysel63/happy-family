5ce1f5e1a50eeaffd3dd40991ef761055f859ebc
Adil İsmayılov <ismayilovadil670@gmail.com>
2024-10-12 11:23:27 +0400

    hw04 Man and Woman classes created

---
+++ src/main/java/az/edu/turing/Entities/Woman.java
@@ -0,0 +1,21 @@
+package az.edu.turing.Entities;
+
+public class Woman extends Human {
+
+    public Woman(String name, String surname, int year) {
+        super(name, surname, year);
+    }
+
+    public Woman(String name, String surname, int year, int iq, String[][] schedule, Family family) {
+        super(name, surname, year, iq, schedule, family);
+    }
+
+    @Override
+    public String greetPet() {
+        return super.greetPet() + "Let's go for a walk.";
+    }
+
+    public String makeup() {
+        return "Just a second. I have to complete my makeup...";
+    }
+}
