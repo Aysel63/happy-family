77833206f3a4238591fa0433c0faf6efc87ec906
Muhammed Alizada <119886075+alizadamuhammmed@users.noreply.github.com>
2024-10-12 15:34:38 +0400

    HumanCreator interface was added

--- src/main/java/az/edu/turing/Entities/Family.java
+++ src/main/java/az/edu/turing/Entities/Family.java
@@ -2,8 +2,9 @@ package az.edu.turing.Entities;
 
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.Random;
 
-public class Family {
+public class Family implements HumanCreator {
 
     private Human mother;
     private Human father;
@@ -115,5 +116,28 @@ public class Family {
                 ", children=" + Arrays.toString(children) +
                 ", pet=" + pet +
                 '}';
+    }
+
+    @Override
+    public Human bornChild() {
+        Random random = new Random();
+
+        boolean isBoy = random.nextBoolean();
+
+        String[] boyNames = {"Muhammed", "Adil", "Elshan"};
+        String[] girlNames = {"Aysel", "Sevda", "Fidan"};
+        String name = isBoy ? boyNames[random.nextInt(boyNames.length)] : girlNames[random.nextInt(girlNames.length)];
+
+        int averageIq = (mother.getIq() + father.getIq()) / 2;
+
+        Human child;
+        if (isBoy) {
+            child = new Man(name, father.getName(), 2024, averageIq, null, this);
+        } else {
+            child = new Woman(name, father.getName(), 2024, averageIq, null, this);
+        }
+
+        addChild(child);
+        return child;
     }
 }
