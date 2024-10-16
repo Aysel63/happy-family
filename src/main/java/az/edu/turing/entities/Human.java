2d6b3805ed3a2f6d10fa4935bec6c1820e1bea75
Elshan Alasgarov <aleskerof00@gmail.com>
2024-10-17 00:32:32 +0400

    Add describeAge method , constructor for adopted children with birth date parsing and finalize() method

--- src/main/java/az/edu/turing/entities/Human.java
+++ src/main/java/az/edu/turing/entities/Human.java
@@ -2,6 +2,9 @@ package az.edu.turing.entity;
 
 import az.edu.turing.entity.Pet;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -34,6 +37,26 @@ public class Human {
         this.family = family;
     }
 
+    public Human(String name, String surname, String birthDate, int iq) {
+        this.name = name;
+        this.surname = surname;
+        this.birthDate = parseBirthDate(birthDate).toEpochDay();
+        this.iq = iq;
+    }
+
+    private LocalDate parseBirthDate(String birthDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return LocalDate.parse(birthDate, formatter);
+    }
+
+    public String describeAge() {
+        LocalDate birthDateLocal = LocalDate.ofEpochDay(birthDate);
+        LocalDate currentDate = LocalDate.now();
+
+        Period age = Period.between(birthDateLocal, currentDate);
+        return String.format("%d years, %d months, and %d days", age.getYears(), age.getMonths(), age.getDays());
+    }
+
     public String greetPets() {
         List<String>petNickNames=family.getPets().stream().map(Pet::getNickname).colllect(Collectors.toList());
         String result=String.join(", ",petNickNames);
@@ -48,6 +71,12 @@ public class Human {
                     pet.getSpecies(), pet.getAge(), slyLevel));
         }
         return result.toString();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Human object is being removed: " + this.getName() + " " + this.getSurname());
+
     }
 
 
@@ -109,7 +138,7 @@ public class Human {
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, surname,birthDate, family);
+        return Objects.hash(name, surname, birthDate, family);
     }
 
     @Override
