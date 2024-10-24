b1afd4fc386dc2a0c11b2b69dfbe0d49251dbc2f
Adil İsmayılov <ismayilovadil670@gmail.com>
2024-10-24 19:55:39 +0400

    Logger last

--- src/main/java/az/edu/turing/service/Logger.java
+++ src/main/java/az/edu/turing/service/Logger.java
@@ -4,12 +4,14 @@ import az.edu.turing.model.constant.AppConstant;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 
 public class Logger {
 
-    private static final String RESOURCE_PATH = "src/main/java/resource/";
-    private static final String LOGGER_FILE_PATH = RESOURCE_PATH.concat("applicationLog.txt");
+    private static final Path RESOURCE_PATH = Paths.get("src/main/java/resource/");
+    private static final Path LOGGER_FILE_PATH = RESOURCE_PATH.resolve("applicationLog.txt");
 
     public static void info(String message) {
         String log = LocalDateTime.now().format(AppConstant.LOG_FORMATTER).concat(" DEBUG " + message);
@@ -22,7 +24,7 @@ public class Logger {
     }
 
     private static void saveToFile(String log) {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(LOGGER_FILE_PATH, true))) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(LOGGER_FILE_PATH.toString(), true))) {
             bw.write(log + "\n");
         } catch (Exception e) {
             e.printStackTrace();
